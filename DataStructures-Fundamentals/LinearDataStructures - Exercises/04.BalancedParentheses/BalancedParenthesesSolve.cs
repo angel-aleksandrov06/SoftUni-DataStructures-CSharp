39b6ceaeb80b9b1ff607b81463aa61d6562e21a9
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-09-15 23:11:00 +0300

    Add "BalancedParentheses"

--- DataStructures-Fundamentals/LinearDataStructures - Exercises/04.BalancedParentheses/BalancedParenthesesSolve.cs
+++ DataStructures-Fundamentals/LinearDataStructures - Exercises/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -7,7 +7,45 @@
     {
         public bool AreBalanced(string parentheses)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(parentheses) || parentheses.Length % 2 == 1)
+            {
+                return false;
+            }
+
+            Stack<char> openBrackets = new Stack<char>(parentheses.Length / 2);
+
+            foreach (char cureBracket in parentheses)
+            {
+                char expectedChar = default;
+
+                switch (cureBracket)
+                {
+                    case ')':
+                        expectedChar = '(';
+                        break;
+                    case '}':
+                        expectedChar = '{';
+                        break;
+                    case ']':
+                        expectedChar = '[';
+                        break;
+                    default:
+                        openBrackets.Push(cureBracket);
+                        break;
+                }
+
+                if (expectedChar == default)
+                {
+                    continue;
+                }
+
+                if (openBrackets.Pop() != expectedChar)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
