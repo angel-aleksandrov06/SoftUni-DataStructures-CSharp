fc68c3cd4358f5d4920bb11ab7d881c389067d5e
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-09-25 23:08:03 +0300

    add 02.LowestCommonAncestor
    Done

--- DataStructures-Fundamentals/Heaps-BST - Exercises/02.LowestCommonAncestor/BinaryTree.cs
+++ DataStructures-Fundamentals/Heaps-BST - Exercises/02.LowestCommonAncestor/BinaryTree.cs
@@ -1,6 +1,7 @@
 ﻿namespace _02.LowestCommonAncestor
 {
     using System;
+    using System.Collections.Generic;
 
     public class BinaryTree<T> : IAbstractBinaryTree<T>
         where T : IComparable<T>
@@ -35,7 +36,47 @@
 
         public T FindLowestCommonAncestor(T first, T second)
         {
-            throw new NotImplementedException();
+            var firstList = new List<BinaryTree<T>>();
+            var secondList = new List<BinaryTree<T>>();
+
+            this.FindNodeDFS(this, first, firstList);
+            this.FindNodeDFS(this, second, secondList);
+
+            var firstNode = firstList[0];
+            var secondNode = secondList[0];
+
+            T parentToLookFor = firstNode.Parent.Value;
+            while (!parentToLookFor.Equals(firstNode.Value) || !parentToLookFor.Equals(secondNode.Value))
+            {
+                if (!parentToLookFor.Equals(firstNode.Value))
+                {
+                    firstNode = firstNode.Parent;
+                }
+
+                if (!parentToLookFor.Equals(secondNode.Value))
+                {
+                    secondNode = secondNode.Parent;
+                }
+            }
+
+            return firstNode.Value;
+        }
+
+        private void FindNodeDFS(BinaryTree<T> currNode, T lookUpValue, List<BinaryTree<T>> list)
+        {
+            if (currNode == null)
+            {
+                return;
+            }
+
+            if (currNode.Value.Equals(lookUpValue))
+            {
+                list.Add(currNode);
+                return;
+            }
+
+            this.FindNodeDFS(currNode.LeftChild, lookUpValue, list);
+            this.FindNodeDFS(currNode.RightChild, lookUpValue, list);
         }
     }
 }
