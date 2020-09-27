2211030bf8b87aa3c8312c40a3fc5e80edd6aaac
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-09-27 14:08:28 +0300

    Add exercises {03, 04, 05} to "Heaps-BST - Exercises"
    03.MinHeap 100/100
    04.CookiesProblem 100/100
    05.TopView 100/100

--- DataStructures-Fundamentals/Heaps-BST - Exercises/03.MinHeap/MinHeap.cs
+++ DataStructures-Fundamentals/Heaps-BST - Exercises/03.MinHeap/MinHeap.cs
@@ -13,21 +13,111 @@
             this._elements = new List<T>();
         }
 
-        public int Size => throw new NotImplementedException();
+        public int Size => this._elements.Count;
 
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            var firstElement = this.Peek();
+
+            this.Swap(0, this.Size - 1);
+            this._elements.RemoveAt(this.Size - 1);
+            this.heapifyDown();
+
+            return firstElement;
+        }
+
+        private void heapifyDown()
+        {
+            var currIndex = 0;
+            var leftChildIndex = this.GetLeftChildIndex(currIndex);
+
+            while (this.IsIndexValid(leftChildIndex) && this.IsGreater(currIndex, leftChildIndex))
+            {
+                int toSwapWith = leftChildIndex;
+                int rightChildIndex = this.GetRightChildIndex(currIndex);
+
+                if (this.IsIndexValid(rightChildIndex) && this.IsGreater(toSwapWith, rightChildIndex))
+                {
+                    toSwapWith = rightChildIndex;
+                }
+
+                this.Swap(toSwapWith, currIndex);
+                currIndex = toSwapWith;
+                leftChildIndex = this.GetLeftChildIndex(currIndex);
+            }
         }
 
         public void Add(T element)
         {
-            throw new NotImplementedException();
+            this._elements.Add(element);
+
+            this.heapifyUp();
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+
+            return this._elements[0];
+        }
+
+        private void heapifyUp()
+        {
+            int currIndex = this.Size - 1;
+            var parentIndex = this.GetParentIndex(currIndex);
+
+            while (this.IsIndexValid(currIndex) && this.IsLess(currIndex, parentIndex))
+            {
+                this.Swap(currIndex, parentIndex);
+
+                currIndex = parentIndex;
+                parentIndex = this.GetParentIndex(currIndex);
+            }
+        }
+
+        private void Swap(int currIndex, int parentIndex)
+        {
+            var temp = this._elements[currIndex];
+            this._elements[currIndex] = this._elements[parentIndex];
+            this._elements[parentIndex] = temp;
+        }
+
+        private int GetParentIndex(int childIndex)
+        {
+            return (childIndex - 1) / 2;
+        }
+
+        private int GetLeftChildIndex(int parentIndex)
+        {
+            return 2 * parentIndex +1;
+        }
+
+        private int GetRightChildIndex(int parentIndex)
+        {
+            return 2 * parentIndex + 2;
+        }
+
+        private void EnsureNotEmpty()
+        {
+            if (this.Size == 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        private bool IsIndexValid(int index)
+        {
+            return index >= 0 && index < this.Size;
+        }
+
+        private bool IsGreater(int childIndex, int parentIndex)
+        {
+            return this._elements[childIndex].CompareTo(this._elements[parentIndex]) > 0;
+        }
+
+        private bool IsLess(int childIndex, int parentIndex)
+        {
+            return this._elements[childIndex].CompareTo(this._elements[parentIndex]) < 0;
         }
     }
 }
--- DataStructures-Fundamentals/Heaps-BST - Exercises/04.CookiesProblem/CookiesProblem.cs
+++ DataStructures-Fundamentals/Heaps-BST - Exercises/04.CookiesProblem/CookiesProblem.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Wintellect.PowerCollections;
 
 namespace _04.CookiesProblem
 {
@@ -6,7 +7,30 @@ namespace _04.CookiesProblem
     {
         public int Solve(int k, int[] cookies)
         {
-            throw new NotImplementedException();
+            var bag = new OrderedBag<int>();
+
+            foreach (var cookie in cookies)
+            {
+                bag.Add(cookie);
+            }
+
+            var currMinSweetCookie = bag.GetFirst();
+            int steps = 0;
+
+            while (currMinSweetCookie < k && bag.Count > 1)
+            {
+                var minSweetCookie = bag.RemoveFirst();
+                var beforeMinSweetCookie = bag.RemoveFirst();
+
+                var newCookie = minSweetCookie + (2 * beforeMinSweetCookie);
+
+                bag.Add(newCookie);
+
+                currMinSweetCookie = bag.GetFirst();
+                steps++;
+            }
+
+            return currMinSweetCookie < k ? -1 : steps;
         }
     }
 }
