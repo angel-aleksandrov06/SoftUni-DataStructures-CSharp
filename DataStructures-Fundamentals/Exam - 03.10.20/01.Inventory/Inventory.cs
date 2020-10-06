25714e323d89610d7be7e3ec6b027e9b48e45403
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-10-06 23:29:48 +0300

    Add Exam 03.10.20
    100/100

---
+++ DataStructures-Fundamentals/Exam - 03.10.20/01.Inventory/Inventory.cs
@@ -0,0 +1,178 @@
+﻿namespace _01.Inventory
+{
+    using _01.Inventory.Interfaces;
+    using _01.Inventory.Models;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Inventory : IHolder
+    {
+        private List<IWeapon> weapons;
+
+        public Inventory()
+        {
+            this.weapons = new List<IWeapon>();
+        }
+
+        public int Capacity => this.weapons.Count;
+
+        public void Add(IWeapon weapon)
+        {
+            this.weapons.Add(weapon);
+        }
+
+        public void Clear()
+        {
+            this.weapons.Clear();
+        }
+
+        public bool Contains(IWeapon weapon)
+        {
+            return this.GetIndexById(weapon.Id) != -1;
+        }
+
+        public void EmptyArsenal(Category category)
+        {
+            for (int i = 0; i < this.Capacity; i++)
+            {
+                if (this.weapons[i].Category.Equals(category))
+                {
+                    this.weapons[i].Ammunition = 0;
+                }
+            }
+        }
+
+        public bool Fire(IWeapon weapon, int ammunition)
+        {
+            var index = this.GetIndexById(weapon.Id);
+            this.ValidateIndex(index);
+
+            var currAmmunition = this.weapons[index].Ammunition;
+
+            if (currAmmunition >= ammunition)
+            {
+                this.weapons[index].Ammunition = currAmmunition - ammunition;
+                return true;
+            }
+
+            return false;
+        }
+
+        public IWeapon GetById(int id)
+        {
+            var currIndex = this.GetIndexById(id);
+
+            if (currIndex == -1)
+            {
+                return null;
+            }
+            else
+            {
+                var currElement = this.weapons[currIndex];
+                return currElement;
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return this.weapons.GetEnumerator();
+        }
+
+        public int Refill(IWeapon weapon, int ammunition)
+        {
+            var index = this.GetIndexById(weapon.Id);
+            this.ValidateIndex(index);
+
+            var maxCapacity = this.weapons[index].MaxCapacity;
+            var currAmmunition = this.weapons[index].Ammunition;
+
+            if (currAmmunition < maxCapacity)
+            {
+                if (currAmmunition + ammunition <= maxCapacity)
+                {
+                    this.weapons[index].Ammunition = currAmmunition + ammunition;
+                    return this.weapons[index].Ammunition;
+                }
+                else
+                {
+                    this.weapons[index].Ammunition = maxCapacity;
+                    return this.weapons[index].Ammunition;
+                }
+            }
+
+            return this.weapons[index].Ammunition;
+        }
+
+        public IWeapon RemoveById(int id)
+        {
+            var currIndex = this.GetIndexById(id);
+            this.ValidateIndex(currIndex);
+
+            var currElement = this.weapons[currIndex];
+            this.weapons.RemoveAt(currIndex);
+            return currElement;
+        }
+
+        public int RemoveHeavy()
+        {
+            return this.weapons.RemoveAll(x => x.Category.Equals(Category.Heavy));
+        }
+
+        public List<IWeapon> RetrieveAll()
+        {
+            return new List<IWeapon>(this.weapons);
+        }
+
+        public List<IWeapon> RetriveInRange(Category lower, Category upper)
+        {
+            var result = new List<IWeapon>(this.Capacity);
+
+            for (int i = 0; i < this.Capacity; i++)
+            {
+                if (this.weapons[i].Category >= lower && this.weapons[i].Category <= upper)
+                {
+                    result.Add(this.weapons[i]);
+                }
+            }
+
+            return result;
+        }
+
+        public void Swap(IWeapon firstWeapon, IWeapon secondWeapon)
+        {
+            int indexOfFirstEntity = this.GetIndexById(firstWeapon.Id);
+            this.ValidateIndex(indexOfFirstEntity);
+            int indexOfSecondEntity = this.GetIndexById(secondWeapon.Id);
+            this.ValidateIndex(indexOfSecondEntity);
+
+            if (this.weapons[indexOfFirstEntity].Category == this.weapons[indexOfSecondEntity].Category)
+            {
+                var temp = this.weapons[indexOfFirstEntity];
+                this.weapons[indexOfFirstEntity] = this.weapons[indexOfSecondEntity];
+                this.weapons[indexOfSecondEntity] = temp;
+            }
+        }
+
+        private int GetIndexById(int id)
+        {
+            for (int i = 0; i < this.Capacity; i++)
+            {
+                if (this.weapons[i].Id == id)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        private void ValidateIndex(int currIndex)
+        {
+            if (currIndex < 0 || currIndex >= this.Capacity)
+            {
+                throw new InvalidOperationException("Weapon does not exist in inventory!");
+            }
+        }
+    }
+}
---
+++ DataStructures-Fundamentals/Exam - 03.10.20/02.LegionSystem/Legion.cs
@@ -0,0 +1,199 @@
+﻿namespace _02.LegionSystem
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using _02.LegionSystem.Interfaces;
+    using Wintellect.PowerCollections;
+
+    public class Legion : IArmy
+    {
+        private OrderedSet<IEnemy> enemies;
+
+        public Legion()
+        {
+            this.enemies = new OrderedSet<IEnemy>();
+        }
+
+        public int Size => this.enemies.Count;
+
+        public bool Contains(IEnemy enemy)
+        {
+            return this.enemies.Contains(enemy);
+        }
+
+        public void Create(IEnemy enemy)
+        {
+            this.enemies.Add(enemy);
+        }
+
+        public IEnemy GetByAttackSpeed(int speed)
+        {
+            var indexOfEnemy = this.GetIndexByAttSpeed(speed);
+
+            if (indexOfEnemy == -1)
+            {
+                return null;
+            }
+
+            return this.enemies[indexOfEnemy];
+        }
+
+        public List<IEnemy> GetFaster(int speed)
+        {
+            //var result = new List<IEnemy>(this.Size);
+
+            var result2 = this.enemies.FindAll(x => x.AttackSpeed > speed).ToList();
+
+            //for (int i = 0; i < this.Size; i++)
+            //{
+            //    if (this.enemies[i].AttackSpeed > speed)
+            //    {
+            //        result.Add(this.enemies[i]);
+            //    }
+            //    else
+            //    {
+            //        return result;
+            //    }
+            //}
+
+            return result2;
+        }
+
+        public IEnemy GetFastest()
+        {
+            this.EnsureNotEmpty();
+            return this.enemies.GetFirst();
+        }
+
+        public IEnemy[] GetOrderedByHealth()
+        {
+            if (this.Size == 0)
+            {
+                return new IEnemy[0];
+            }
+
+            List<IEnemy> orderedByHealth = MergeSort(new List<IEnemy>(this.enemies));
+
+
+            return orderedByHealth.ToArray();
+        }
+
+        public List<IEnemy> GetSlower(int speed)
+        {
+            var result2 = this.enemies.FindAll(x => x.AttackSpeed < speed).ToList();
+
+            //var result = new List<IEnemy>(this.Size);
+
+            //for (int i = this.Size - 1; i >= 0; i--)
+            //{
+            //    if (this.enemies[i].AttackSpeed < speed)
+            //    {
+            //        result.Add(this.enemies[i]);
+            //    }
+            //    else
+            //    {
+            //        return result;
+            //    }
+            //}
+
+            return result2;
+        }
+
+        public IEnemy GetSlowest()
+        {
+            this.EnsureNotEmpty();
+            return this.enemies.GetLast();
+        }
+
+        public void ShootFastest()
+        {
+            this.EnsureNotEmpty();
+            this.enemies.RemoveFirst();
+        }
+
+        public void ShootSlowest()
+        {
+            this.EnsureNotEmpty();
+            this.enemies.RemoveLast();
+        }
+
+        private int GetIndexByAttSpeed(int speed)
+        {
+            for (int i = 0; i < this.Size; i++)
+            {
+                if (this.enemies[i].AttackSpeed == speed)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        private void EnsureNotEmpty()
+        {
+            if (this.Size == 0)
+            {
+                throw new InvalidOperationException("Legion has no enemies!");
+            }
+        }
+
+        private static List<IEnemy> MergeSort(List<IEnemy> unsorted)
+        {
+            if (unsorted.Count <= 1)
+                return unsorted;
+
+            List<IEnemy> left = new List<IEnemy>();
+            List<IEnemy> right = new List<IEnemy>();
+
+            int middle = unsorted.Count / 2;
+            for (int i = 0; i < middle; i++)  //Dividing the unsorted list
+            {
+                left.Add(unsorted[i]);
+            }
+            for (int i = middle; i < unsorted.Count; i++)
+            {
+                right.Add(unsorted[i]);
+            }
+
+            left = MergeSort(left);
+            right = MergeSort(right);
+            return Merge(left, right);
+        }
+
+        private static List<IEnemy> Merge(List<IEnemy> left, List<IEnemy> right)
+        {
+            List<IEnemy> result = new List<IEnemy>();
+
+            while (left.Count > 0 || right.Count > 0)
+            {
+                if (left.Count > 0 && right.Count > 0)
+                {
+                    if (left.First().Health >= right.First().Health)  //Comparing First two elements to see which is smaller
+                    {
+                        result.Add(left.First());
+                        left.Remove(left.First());      //Rest of the list minus the first element
+                    }
+                    else
+                    {
+                        result.Add(right.First());
+                        right.Remove(right.First());
+                    }
+                }
+                else if (left.Count > 0)
+                {
+                    result.Add(left.First());
+                    left.Remove(left.First());
+                }
+                else if (right.Count > 0)
+                {
+                    result.Add(right.First());
+
+                    right.Remove(right.First());
+                }
+            }
+            return result;
+        }
+    }
+}
