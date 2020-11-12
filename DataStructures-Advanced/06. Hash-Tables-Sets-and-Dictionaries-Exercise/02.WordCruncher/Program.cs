1db918a89ed7f776f41095d8c0bf249ce8c0c2f3
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-11-12 19:26:45 +0200

    Add "WordCruncher"
    WordCruncher 100/100 Done.

---
+++ DataStructures-Advanced/06. Hash-Tables-Sets-and-Dictionaries-Exercise/02.WordCruncher/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _02.WordCruncher
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(", ");
+            var target = Console.ReadLine();
+
+            WordCruncher wc = new WordCruncher(input, target);
+
+            foreach (var path in wc.GetPaths())
+            {
+                Console.WriteLine(path);
+            }
+        }
+    }
+}
